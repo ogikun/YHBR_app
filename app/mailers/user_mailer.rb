ddddd2f77c93551aaef87c48470f6af5dddf0736
Ogino Kazuya <g1621362@gmail.com>
2021-01-31 22:49:19 +0900

    [fix]バグ修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,15 +2,15 @@ class UserMailer < ApplicationMailer
   def report_email(report)
     @user = User.find(report.reportee_id)
     @report = report
-    mail to: @user.email, subject: 'あなたは通報されました'
+    mail from: ENV["GMAIL_NAME"], to: @user.email, subject: 'あなたは通報されました'
   end
-  
+
   def reflection_email(report, message)
     @user = User.find(report.user_id)
     @report = report
     @reportee = User.find(report.reportee_id)
     @message = message
-    mail to: @user.email, subject: "#{@reportee.name}様が反省しました"
+    mail from: ENV["GMAIL_NAME"], to: @user.email, subject: "#{@reportee.name}様が反省しました"
   end
 
   def report_a_report(report)
