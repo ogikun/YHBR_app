96bed0ef9b28ccc33774a7df0a2d0552537fc486
Ogino Kazuya <g1621362@gmail.com>
2021-02-01 13:37:12 +0900

    [fix] .env

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def report_email(report)
     @user = User.find(report.reportee_id)
     @report = report
-    mail from: ENV["GMAIL_NAME"], to: @user.email, subject: 'あなたは通報されました'
+    mail to: @user.email, subject: 'あなたは通報されました'
   end
 
   def reflection_email(report, message)
@@ -10,13 +10,13 @@ class UserMailer < ApplicationMailer
     @report = report
     @reportee = User.find(report.reportee_id)
     @message = message
-    mail from: ENV["GMAIL_NAME"], to: @user.email, subject: "#{@reportee.name}様が反省しました"
+    mail to: @user.email, subject: "#{@reportee.name}様が反省しました"
   end
 
   def report_a_report(report)
     @user = User.find(report.user_id)
     @reportee = User.find(report.reportee_id)
     @report = report
-    mail to: ENV["GMAIL_NAME"], subject: "不適切な通報"
+    mail to: ENV['GMAIL_NAME'], subject: "不適切な通報"
   end
 end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV["GMAIL_NAME"],
-    password:             ENV["GMAIL_PASS"],
+    user_name:            ENV['GMAIL_NAME'],
+    password:             ENV['GMAIL_PASS'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
